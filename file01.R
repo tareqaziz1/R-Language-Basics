7564914db08f85e493749f680f08b1a0984a3172
TAREQ AZIZ <67520168+tareqaziz1@users.noreply.github.com>
2026-04-14 08:19:17 +0200

    file01.R added

---
+++ file01.R
@@ -0,0 +1,45 @@
+# Assigning Variables in R
+
+x <- 10
+y = 20
+
+# Data Types with Examples
+
+# 1. Numeric (Decimal numbers
+
+price <- 10.5
+temperature <- 36.6
+
+class(price)
+
+# 2. Integer
+
+age <- 25L
+quantity <- 100L
+
+class(age)
+
+# 3. Character
+
+name <- "Tareq"
+city <- "Mechelen"
+
+class(name)
+
+# 4. Logical
+
+is_student <- TRUE
+has_job <- FALSE
+
+class(is_student)
+
+# 5. Complex (Real + Imaginary)
+
+z <- 2 + 3i
+
+class(z)
+
+# To check the type - 
+
+typeof(x)
+class(x)
