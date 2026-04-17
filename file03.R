9864d6f62ed3b230f1ed38c98ad8a8f92498570b
TAREQ AZIZ <67520168+tareqaziz1@users.noreply.github.com>
2026-04-17 21:30:06 +0200

    file03.R added

---
+++ file03.R
@@ -0,0 +1,26 @@
+#Control Flow in R
+
+# Condition -
+
+If-else
+x <- 10
+if(x > 5){
+  print("Big")
+} else {
+  print("Small")
+}
+
+# for and while loop
+
+
+#for loop
+for(i in 1:5){
+  print(i)
+}
+
+while loop
+i <- 1
+while(i <= 5){
+  print(i)
+  i <- i + 1
+}
